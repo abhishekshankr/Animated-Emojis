5ace33cc964041d0c5cd7b59bffd866e8549e0d2
abhishekshankr <abhishekshankar241@gmail.com>
2024-04-06 01:11:15 -0400

    Updated UI and added all emojis

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,10 @@
 // Ensure you have the Figma typings available for better development experience
 // This might involve installing @figma/plugin-typings
 
-figma.showUI(__html__, { width: 800, height: 600 });
+figma.showUI(__html__, { width: 700, height: 800 });
 
 // Listen for messages sent from the plugin UI
-figma.ui.onmessage = async (msg: { type: string; imageUrl?: string }) => {
+figma.ui.onmessage = async (msg: { type: string; imageUrl?: string; size: number, name: string }) => {
     if (msg.type === 'add-emoji' && msg.imageUrl) {
         try {
             // Fetch the GIF image
@@ -22,7 +22,8 @@ figma.ui.onmessage = async (msg: { type: string; imageUrl?: string }) => {
             
             // Create a rectangle to hold the GIF
             const rect = figma.createRectangle();
-            rect.resize(512, 512); // Set the size to 512x512
+            rect.name = msg.name;
+            rect.resize(msg.size, msg.size); // Set the size to 512x512
             rect.fills = [fill]; // Apply the image fill
             
             // Add the rectangle to the current page
